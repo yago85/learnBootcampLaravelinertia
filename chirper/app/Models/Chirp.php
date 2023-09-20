eb6ef7f9698ff9d2c468ebbf98cbcd054e58201a
karcev <karcev@gmail.com>
2023-09-20 23:31:21 +0300

    learn Bootcamp Larave linertia

---
+++ chirper/app/Models/Chirp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    // включить массовое присвоение атрибута message
+    protected $fillable = [
+        'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ chirper/resources/js/Pages/Chirps/Index.vue
@@ -0,0 +1,43 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Chirp from '@/Components/Chirp.vue';
+import InputError from '@/Components/InputError.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { useForm, Head } from '@inertiajs/vue3';
+
+defineProps(['chirps']);
+
+const form = useForm({
+    message: '',
+});
+</script>
+
+<template>
+    <Head title="Chirps" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Chirps</h2>
+        </template>
+        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
+                <textarea
+                    v-model="form.message"
+                    placeholder="Что хотел сказать?"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                ></textarea>
+                <InputError :message="form.errors.message" class="mt-2" />
+                <PrimaryButton class="mt-4">Написать</PrimaryButton>
+            </form>
+
+            <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                <Chirp
+                    v-for="chirp in chirps"
+                    :key="chirp.id"
+                    :chirp="chirp"
+                />
+            </div>
+
+        </div>
+    </AuthenticatedLayout>
+</template>
